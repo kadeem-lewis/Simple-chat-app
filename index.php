e7b10e1de766717d4e5cf724749e1f28a9e83ec3
kadeem-lewis <print.helloworld@genericindividual.com>
2022-12-15 03:33:38 -0500

    added error messages

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <?php
 
-require 'functions.php';
+require_once 'functions.php';
+$error_message;
+$success_message;
+$_SESSION["logged_in"] = false;
 
+if (isset($_POST['Submit'])) {
+    $username = htmlspecialchars(trim($_POST["user-name"]));
+    $password = htmlspecialchars(trim($_POST["user-password"]));
 
-// TODO: login verification and validation
-
-
+    if (login($conn, $username, $password)) {
+        $success_message = "Login successful.";
+        $_SESSION["logged_in"] = true;
+        $_SESSION["username"] = $username;
+    } else {
+        $error_message = "Invalid username or password.";
+        $_SESSION["logged_in"] = false;
+    }
+}
 // TODO: sending typed messages to database
 
 
