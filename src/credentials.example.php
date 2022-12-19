bd2e404f67efbef52409c74e358ed44fec03c785
kadeem-lewis <print.helloworld@genericindividual.com>
2022-12-19 00:32:44 -0500

    created separate config file

---
+++ src/credentials.example.php
@@ -0,0 +1,14 @@
+<?php
+
+// return [
+//     'database' => [
+//         'host' => 'localhost',
+//         'username' => 'root',
+//         'password' => '',
+//         'dbname' => 'testdb',
+//         'options' => [
+//             PDO::ATTR_PERSISTENT => true,
+//             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+//         ]
+//     ],
+// ];
--- src/models/db.php
+++ src/models/db.php
@@ -1,20 +1,17 @@
 <?php
 //Makes DB connection
+$config = require '../config/database.php';
 $servername = "sql1.njit.edu";
 $username = "ksl29";
 $password = "MrJoestur@69";
 $dbname = "ksl29";
 $error;
 
-$dsn = 'mysql:host=' . $servername . ';dbname=' . $dbname;
-$options = [
-    PDO::ATTR_PERSISTENT => true,
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-];
+$dsn = 'mysql:host=' . $config['database']['host'] . ';dbname=' . $config['database']['dbname'];
 
 // Create PDO Instance
 try {
-    $conn = new PDO($dsn, $username, $password, $options);
+    $conn = new PDO($dsn, $config['database']['username'], $config['database']['password'], $config['database']['password']);
 } catch (PDOException $e) {
     $error = $e->getMessage();
     echo $error;
